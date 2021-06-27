9ac6e0f5a44b9e406211ece2829ce3b1dbdff767
Mélanie Da Costa <1161655-M3L_DC@users.noreply.gitlab.com>
2021-06-27 13:00:38 +0200

    modification de la page catalogue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="game-card mr-3 ml-3">
-    <b-card no-body class="overflow-hidden" style="max-width: 20rem">
+  <div class="game-card">
+    <b-card no-body class="overflow-hidden body-game-card">
       <b-card
         overlay
         :title="title"
@@ -9,7 +9,7 @@
         img-top
         class="mb-2"
       ></b-card>
-      <b-button href="#" variant="dark">Play</b-button>
+      <button class="btn-play" href="#" variant="dark">Play</button>
     </b-card>
   </div>
 </template>
@@ -26,5 +26,16 @@ export default {
   flex-direction: row;
   margin-left: 15px;
   margin-right: 15px;
+}
+.body-game-card {
+  max-width: 20rem !important;
+  background: none !important;
+  border: none !important;
+}
+.btn-play {
+  background-color: #ff0000 !important;
+  color: rgb(233, 231, 231);
+  border: none;
+  border-radius: 20px;
 }
 </style>
--- src/views/CatalogueView.vue
+++ src/views/CatalogueView.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="catalogue">
-    <h1>Catalogue</h1>
+  <div class="pt-5">
+    <h1 style="color: rgb(233, 231, 231)">Catalogue</h1>
     <div class="list-game">
       <Card title="GAME 1" imgUrl="https://picsum.photos/600/300/?image=25" />
       <Card title="GAME 2" imgUrl="https://picsum.photos/600/300/?image=25" />
-      <Card title="GAME 3" imgUrl="https://picsum.photos/600/300/?image=25" />
     </div>
   </div>
 </template>
@@ -22,6 +21,7 @@ export default {
 <style lang="scss">
 .list-game {
   display: flex !important;
-  margin: 50px 50px;
+  margin: 50px;
+  justify-content: center;
 }
 </style>
